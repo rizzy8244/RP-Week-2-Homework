a099c340622814fe8c0d7af4532e32ddcfa326b5
rizzy8244 <rperminter1@yahoo.com>
2022-11-10 16:08:22 -0500

    Week 2 HW

---
+++ WK2HW.js
@@ -0,0 +1,28 @@
+//const { type } = require("os");
+
+let pizzaPlace;
+let numberOfToppings;
+
+pizzaPlace = 'Paisanos';
+numberOfToppings = 4;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(`Welcome to ${pizzaPlace} we have ${numberOfToppings} toppings for all of our selections.`);
+
+if (numberOfToppings<10) {
+  console.log("Quality, not quantity.");
+} else {
+console.log("A whole lot of pizza.");
+}
+
+
+for (let i = 1; i <10; i+=) {
+  console.log(i);
+
+}
+
